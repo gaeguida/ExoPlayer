b5fe5156dd602d010b5d6e77ba5e1ff613df6d7c
Domenico Di Caro <d.dicaro@ideasolutions.it>
2021-11-26 12:41:38 +0100

    MediaCodecVideoRenderer: added device 'HWCLT' to method handling output surface workaround.

--- library/core/src/main/java/com/google/android/exoplayer2/video/MediaCodecVideoRenderer.java
+++ library/core/src/main/java/com/google/android/exoplayer2/video/MediaCodecVideoRenderer.java
@@ -1594,6 +1594,7 @@ public class MediaCodecVideoRenderer extends MediaCodecRenderer {
   }
 
   private static boolean evaluateDeviceNeedsSetOutputSurfaceWorkaround() {
+    android.util.Log.d("RTI-check", "evaluateDeviceNeedsSetOutputSurfaceWorkaround");
     if (Util.SDK_INT <= 28) {
       // Workaround for MiTV and MiBox devices which have been observed broken up to API 28.
       // https://github.com/google/ExoPlayer/issues/5169,
@@ -1613,9 +1614,10 @@ public class MediaCodecVideoRenderer extends MediaCodecRenderer {
           break; // Do nothing.
       }
     }
-    if (Util.SDK_INT <= 27 && "HWEML".equals(Util.DEVICE)) {
+    if (Util.SDK_INT <= 27 && ("HWEML".equals(Util.DEVICE) || "HWCLT".equals(Util.DEVICE))) {
       // Workaround for Huawei P20:
       // https://github.com/google/ExoPlayer/issues/4468#issuecomment-459291645.
+      android.util.Log.d("RTI-check", "evaluateDeviceNeedsSetOutputSurfaceWorkaround apply workaround return true");
       return true;
     }
     if (Util.SDK_INT <= 26) {
@@ -1896,6 +1898,7 @@ public class MediaCodecVideoRenderer extends MediaCodecRenderer {
           break; // Do nothing.
       }
     }
+    android.util.Log.d("RTI-check", "evaluateDeviceNeedsSetOutputSurfaceWorkaround don't apply workaround return true");
     return false;
   }
 
